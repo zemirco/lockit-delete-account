7fbf9956474bbd192ada93b956a4aa09f4df070d
zeMirco <mirco.zeiss@gmail.com>
2014-01-24 18:26:44 +0100

    use new structure for config

--- test/config.js
+++ test/config.js
@@ -1,4 +1,15 @@
 
 // settings for test
-exports.db = 'couchdb';
-exports.dbUrl = 'http://127.0.0.1:5984/test';
+exports.db = 'http://127.0.0.1:5984/test';
+
+exports.signup = {
+  tokenExpiration: '1 day'
+};
+
+exports.deleteAccount = {
+  route: '/delete-account',
+  views: {
+    remove: '',         // input fields 'username', 'phrase', 'password' | POST /'deleteAccount.route' | local variable 'error'
+    removed: ''         // message that account has been deleted
+  }
+};
