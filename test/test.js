ae84ee274b2045f80bbbcfe64efeeb2317320d02
zeMirco <mirco.zeiss@gmail.com>
2014-01-24 18:28:15 +0100

    tests for custom views

--- test/test.js
+++ test/test.js
@@ -1,11 +1,22 @@
 
 var request = require('supertest');
 var should = require('should');
-
-var app = require('./app.js');
+var utls = require('lockit-utils');
 
 var config = require('./config.js');
-var adapter = require('lockit-' + config.db + '-adapter')(config);
+var app = require('./app.js')(config);
+
+var db = utls.getDatabase(config);
+var adapter = require(db.adapter)(config);
+
+// create a second app for testing custom views
+var config_2 = JSON.parse(JSON.stringify(config));
+config_2.port = 4000;
+config_2.deleteAccount.views = {
+  remove: 'custom/remove.jade',
+  removed: 'custom/removed.jade'
+};
+var app_2 = require('./app.js')(config_2);
 
 // add a dummy user to db
 before(function(done) {
@@ -27,6 +38,17 @@ describe('delete account', function() {
           res.statusCode.should.equal(200);
           res.text.should.include('Once you delete your account, there is no going back');
           res.text.should.include('<title>Delete account</title>');
+          done();
+        });
+
+    });
+
+    it('should work with custom views', function(done) {
+
+      request(app_2)
+        .get('/delete-account')
+        .end(function(err, res) {
+          res.text.should.include('Please don\'t go!');
           done();
         });
 
@@ -87,6 +109,18 @@ describe('delete account', function() {
         });
       
     });
+
+    it('should work with custom error view', function(done) {
+
+      request(app_2)
+        .post('/delete-account')
+        .send({username: 'john', phrase: 'please delete my account forever', password: 'secret'})
+        .end(function(error, res) {
+          res.text.should.include('Please don\'t go!');
+          done();
+        });
+
+    });
     
     it('should delete a user from db when everything is fine', function(done) {
 
@@ -100,6 +134,28 @@ describe('delete account', function() {
           done();
         });
       
+    });
+
+    it('should work with custom success view', function(done) {
+      
+      // reactivate user - has to be the same because of current session
+      // session is set before middleware so it is renewed although destroyed 
+      // by test before
+      before(function(done) {
+        adapter.save('john', 'john@email.com', 'password', function(err, user) {
+          if (err) console.log(err);
+          done();
+        });
+      });
+
+      request(app_2)
+        .post('/delete-account')
+        .send({username: 'john', phrase: 'please delete my account forever', password: 'password'})
+        .end(function(error, res) {
+          res.text.should.include('We will miss you!');
+          done();
+        });
+
     });
     
   });
